5b790a666f9deda87b7baabd462676cd1ddca315
Nathan Warner <nathan@frcv.net>
2022-08-24 22:21:04 -0600

    Adding some db stuff

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -3,4 +3,5 @@ from fastapi import APIRouter
 from api.api_v1.endpoints import users
 
 api_router = APIRouter()
-api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(users.router, tags=["users"])
+#api_router.include_router(users.router, prefix="/users", tags=["users"])
--- app/api/api_v1/endpoints/users.py
+++ app/api/api_v1/endpoints/users.py
@@ -1,7 +1,34 @@
-from fastapi import APIRouter
+from fastapi import Depends, HTTPException, APIRouter
+from sqlalchemy.orm import Session
+
+from db import models, schemas
+from db.database import SessionLocal, engine
+from api.api_v1 import crud
+
+models.Base.metadata.create_all(bind=engine)
 
 router = APIRouter()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @router.get("/me")
 def read_user_me():
     return "me"
+
+@router.post("/users", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+
+@router.get("/users", response_model=list[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
